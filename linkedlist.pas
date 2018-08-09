799b6ba753127e0a9f54e414856c6a7dc7ef7113
Elina Eickstädt <elina.eickstaedt@gmail.com>
2018-08-09 08:32:47 +0200

    Initial commit

---
+++ linkedlist.pas
@@ -0,0 +1,17 @@
+program VerketteteListe;
+begin 
+{ Hilfsprozeduren}
+
+
+
+ 
+{Test Prozedure}
+
+
+{Begin hauptporgramm}
+
+{print "LinkedList Play Ground"}
+writeln('LinkedList Playground')
+
+
+end.
